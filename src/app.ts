7a4f528034d26bee0bbfa676448f0b3195d611f8
MehediHasan128 <mehedihasan12926@gmail.com>
2025-03-10 16:44:51 +0600

    server is connect to database successfully

--- src/app.ts
+++ src/app.ts
@@ -5,13 +5,13 @@ import express, { Application } from 'express';
 const app: Application = express()
 
 // Perser
+app.use(express.json());
+app.use(cors());
 
 const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+export default app;
