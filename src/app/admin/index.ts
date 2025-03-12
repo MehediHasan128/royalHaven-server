769da6c154cabe9e67a8ccb0963a6133f4596193
MehediHasan128 <mehedihasan12926@gmail.com>
2025-03-12 11:27:25 +0600

    create admin user info

---
+++ src/app/admin/index.ts
@@ -0,0 +1,23 @@
+import config from "../config";
+import { User } from "../modules/users/user.model";
+
+const admin = {
+  id: 'Admin@2025',
+  userName: { firstName: 'Mehedi Hasan', lastName: 'Baized' },
+  email: 'mehedihasanbaized@gmail.com',
+  password: config.admin_password,
+  role: 'admin',
+  profileImage: '',
+  phoneNumber: '+8801302557956',
+  gender: 'male',
+  status: 'active'
+};
+
+const seedAdmin = async() => {
+    const isAdminExists = await User.findOne({role: 'admin'});
+    if(!isAdminExists){
+        await User.create(admin);
+    };
+};
+
+export default seedAdmin;
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import mongoose from "mongoose";
 import app from "./app";
 import config from "./app/config";
+import seedAdmin from "./app/admin";
 
 async function main() {
   try {
     await mongoose.connect(config.database_url as string);
+
+    seedAdmin();
 
     app.listen(config.port, () => {
         console.log(`Example app listening on port ${config.port}`)
